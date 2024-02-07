df8ea4cb8acf21805580ee5b79d38ccb8f8c3e8f
Oleh Protsak <protsak19@gmail.com>
2024-02-07 17:18:15 +0000

    Implement TodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import TodoForm from "./components/TodoForm"
 
 function App() {
 
 
   return (
     <>
-
+      <TodoForm />
     </>
   )
 }
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,35 @@
+import FormControl from '@mui/material/FormControl';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+
+const TodoForm = () => {
+  return (
+    <Container>
+      <form >
+        <FormControl>
+            <TextField
+                id="standard-basic"
+                label="What you need to do?"
+                variant="filled"
+                required={true}
+
+            />
+            <Button
+                variant='contained' 
+                color='primary' 
+                type='submit'
+                style={{marginTop: 5}}
+            
+            >
+                Add
+            </Button>
+        </FormControl>
+      </form>
+    </Container>
+  )
+}
+
+export default TodoForm
