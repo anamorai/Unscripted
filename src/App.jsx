a0c91e4e8f5dbff412f9b0be06b79be019b3e08c
Anastasia Moraites <anastasiamoraites@gmail.com>
2024-02-09 21:12:20 +0200

    debugged the drag and drop funciton for the to do forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,8 @@
 import { useState } from "react"
 import { v4 } from "uuid";
-import TodoForm from "./components/TodoForm"
-import TodoList from "./components/TodoList"
 import Grid from './components/Grid';
-import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCorners, useSensor, useSensors } from "@dnd-kit/core";
-import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
-
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-    setTodos([...todos, { id: v4(), title: text, isComplited: false }]);
-  };
-
-  const checkTodo = (id) => {
-    setTodos(todos.map(todo => {
-      return todo.id === id ? { ...todo, isComplited: !todo.isComplited } : todo;
-    }))
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => !(todo.id === id)))
-  }
-
-  const getTodoPos = (id) => todos.findIndex(todo => todo.id === id);
-
-  const handleDragEnd = (e) => {
-    const { active, over } = e;
-
-
-    if (active.id === over.id) return;
-
-    setTodos(todos => {
-      const originalPos = getTodoPos(active.id);
-      const newPos = getTodoPos(over.id);
-
-      return arrayMove(todos, originalPos, newPos);
-    })
-
-
-  }
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(TouchSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  )
-
   return (
     <>
       <div>
@@ -59,7 +12,5 @@ function App() {
     </>
   )
 }
-
-
 
 export default App;
--- src/components/TodoComplete.jsx
+++ src/components/TodoComplete.jsx
@@ -2,11 +2,14 @@ import TodoForm from "./TodoForm"
 import TodoList from "./TodoList"
 import { useState } from "react"
 import { v4 } from "uuid";
+import { DndContext, KeyboardSensor, PointerSensor, TouchSensor, closestCorners, useSensor, useSensors } from "@dnd-kit/core";
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 
 
-export default function TodoComplete () {
 
-    const [todos, setTodos] = useState([]);
+export default function TodoComplete() {
+
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (text) => {
     setTodos([...todos, { id: v4(), title: text, isComplited: false }]);
@@ -24,11 +27,35 @@ export default function TodoComplete () {
 
   const getTodoPos = (id) => todos.findIndex(todo => todo.id === id);
 
+  const handleDragEnd = (e) => {
+    const { active, over } = e;
 
-    return (
+
+    if (active.id === over.id) return;
+
+    setTodos(todos => {
+      const originalPos = getTodoPos(active.id);
+      const newPos = getTodoPos(over.id);
+
+      return arrayMove(todos, originalPos, newPos);
+    })
+  }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  )
+
+
+  return (
     <>
-        <TodoList todos={ todos } checkTodo={ checkTodo } deleteTodo={ deleteTodo } />
-      <TodoForm addTodo={ addTodo } />
+      <DndContext sensors={sensors} collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+        <TodoList todos={todos} checkTodo={checkTodo} deleteTodo={deleteTodo} />
+      </DndContext>
+      <TodoForm addTodo={addTodo} />
     </>
   )
 }
