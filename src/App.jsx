24a2b33eea67338c6173a08344fb2e366ca05746
Anastasia Moraites <anastasiamoraites@gmail.com>
2024-02-12 18:52:04 +0200

    implemented a light and dark mode toggle function

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,34 @@ import Grid from './components/Grid';
 import { useEffect } from 'react';
 import axios from "axios";
 import { useState } from 'react';
-
+import { Paper, Switch } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from '@mui/material/useMediaQuery';
 function App() {
   const [diet, setDiet] = useState("");
   const [bodyPart, setBodyPart] = useState("");
   const [dataFromRecipeAPI, setDataFromRecipeAPI] = useState([]);
   const [dataFromTrainingAPI, setDataFromTrainingAPI] = useState([]);
   const [dataFromQuoteAPI, setDataFromQuoteAPI] = useState([]);
-
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [mode, setMode] = useState(prefersDarkMode);
+  const appTheme = createTheme({
+    palette: {
+      mode: mode ? "dark" : "light",
+    },
+  });
+  const handleChange = () => {
+    if (mode) {
+      setMode(false);
+    } else {
+      setMode(true);
+    }
+  };
   const fetchAPI = async (url) => {
     const options = {
       method: "GET",
       url,
   };
-
     try {
       const response = await axios.request(options);
       return response.data;
@@ -24,7 +38,6 @@ function App() {
       console.error(error);
     }
   }
-
   useEffect( () => {
     const fetchRecipeAPI = async () => {
       const data = await fetchAPI(`http://localhost:8000/${diet}`);
@@ -32,7 +45,6 @@ function App() {
     }
     fetchRecipeAPI();
   }, [diet]);
-
   useEffect( () => {
     const fetchTrainingAPI = async () => {
       const data = await fetchAPI(`http://localhost:8000/${bodyPart}`);
@@ -40,25 +52,31 @@ function App() {
     }
     fetchTrainingAPI();
   }, [bodyPart]);
-
   useEffect( () => {
     const fetchingQuoteAPI = async () => {
       const data = await fetchAPI("http://localhost:8000/quote");
       setDataFromQuoteAPI(data);
-    } 
+    }
     fetchingQuoteAPI();
   }, [dataFromQuoteAPI]);
-
   return (
     <>
-          <div style={{ marginBottom: '50px' }}>
+      <ThemeProvider theme={appTheme}>
+    <Paper elevation={0} sx={{ height: "100vh" }} square>
+      <Switch
+        checked={mode}
+        onChange={handleChange}
+        inputProps={{ "aria-label": "controlled" }}
+      />
+        <div style={{ marginBottom: '50px' }}>
         <Header />
       </div>
       <div>
         <Grid diet={diet} setDiet={setDiet} bodyPart={bodyPart} setBodyPart={setBodyPart} dataFromRecipeAPI={dataFromRecipeAPI} dataFromTrainingAPI={dataFromTrainingAPI} />
       </div>
+    </Paper>
+  </ThemeProvider>
     </>
   )
 }
-
 export default App;
