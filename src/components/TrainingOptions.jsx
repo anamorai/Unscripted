77e94e9cf3352c5d7a85116041d88f919a88b442
Anastasia Moraites <anastasiamoraites@gmail.com>
2024-02-13 17:09:14 +0200

    Made changes to training options so that it would display API

--- src/components/TrainingOptions.jsx
+++ src/components/TrainingOptions.jsx
@@ -1,78 +1,112 @@
-import React from 'react';
-import { Grid, Paper, Typography, Checkbox, FormControlLabel, Button, Box } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Grid, Paper, Typography, Checkbox, FormControlLabel, Button, Box, CircularProgress, ListItem } from '@mui/material';
+import axios from "axios";
+import { v4 } from "uuid";
 
 const options = ["Chest", "Back", "Legs", "Abs", "Arms", "Stretching"];
 
 const TrainingOptions = () => {
-  const [selectedOption, setSelectedOption] = React.useState(null);
+  const [bodyPart, setBodyPart] = useState("");
+  const [exercises, setExercises] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    const fetchRecipeAPI = async () => {
+      setLoading(true);
+      const options = {
+        method: 'GET',
+        url: `http://localhost:8000/${bodyPart}`,
+      };
+
+      try {
+        const response = await axios.request(options);
+        setExercises(response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchRecipeAPI();
+  }, [bodyPart]);
 
   const handleToggleOption = (option) => {
-    setSelectedOption(option);
+    setBodyPart(option);
   };
 
   const handleGoBack = () => {
-    setSelectedOption(null);
+    setBodyPart(null);
   };
 
   return (
     <Box
-      sx={{
+      sx={ {
         borderRadius: 8,
-        boxShadow: 3, 
-        p: 2, 
-      }}
+        boxShadow: 3,
+        p: 2,
+      } }
     >
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
+      <Grid container spacing={ 3 }>
+        <Grid item xs={ 12 }>
           <Paper>
             <Grid container justify="space-between" alignItems="center">
-              {selectedOption && (
+              { bodyPart && (
                 <Grid item>
-                  <Button onClick={handleGoBack}>
+                  <Button onClick={ handleGoBack }>
                     Back
                   </Button>
                 </Grid>
-              )}
+              ) }
               <Grid item>
                 <Typography variant="h5" align="center">
-                  {selectedOption ? `Top 3 exercises for ${selectedOption}` : "What would you like to train today?"}
+                  { bodyPart ? `Top 3 exercises for ${bodyPart}` : "What would you like to train today?" }
                 </Typography>
               </Grid>
             </Grid>
           </Paper>
+          { loading && (
+            <Grid item xs={ 12 } align="center">
+              <CircularProgress />
+            </Grid>
+          ) }
+          { bodyPart && !loading && (
+            exercises?.slice(0, 3).map(exercise => <ListItem key={ v4() }> Exercise: { exercise["WorkOut"] }</ListItem>)
+          ) }
         </Grid>
-        {!selectedOption && (
+        { !bodyPart && (
           <>
-            <Grid item xs={6}>
-              <Grid container direction="column" spacing={1}>
-                {options.slice(0, 3).map((option, index) => (
-                  <Grid item key={index}>
+            <Grid item xs={ 6 }>
+              <Grid container direction="column" spacing={ 1 }>
+                { options.slice(0, 3).map((option, index) => (
+                  <Grid item key={ index }>
                     <FormControlLabel
-                      control={<Checkbox checked={selectedOption === option} onChange={() => handleToggleOption(option)} />}
-                      label={option}
-                      style={{ width: '100%' }}
-                      disabled={!!selectedOption}
+                      control={ <Checkbox checked={ bodyPart === option } onChange={ () => handleToggleOption(option) } /> }
+                      label={ option }
+                      style={ { width: '100%' } }
+                      disabled={ !!bodyPart }
                     />
                   </Grid>
-                ))}
+                )) }
               </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <Grid container direction="column" spacing={1}>
-                {options.slice(3).map((option, index) => (
-                  <Grid item key={index}>
+            <Grid item xs={ 6 }>
+              <Grid container direction="column" spacing={ 1 }>
+                { options.slice(3).map((option, index) => (
+                  <Grid item key={ index }>
                     <FormControlLabel
-                      control={<Checkbox checked={selectedOption === option} onChange={() => handleToggleOption(option)} />}
-                      label={option}
-                      style={{ width: '100%' }}
-                      disabled={!!selectedOption}
+                      control={ <Checkbox checked={ bodyPart === option } onChange={ () => handleToggleOption(option) } /> }
+                      label={ option }
+                      style={ { width: '100%' } }
+                      disabled={ !!bodyPart }
                     />
                   </Grid>
-                ))}
+                )) }
               </Grid>
             </Grid>
           </>
-        )}
+        ) }
       </Grid>
     </Box>
   );
